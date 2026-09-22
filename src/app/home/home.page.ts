5e2ccba7dbd11ae5821f13d53af38c181c58dbbe
Alan-gpaz <alangpazr16@gmail.com>
2026-09-22 09:02:01 -0600

    Modificaciones de clase

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular';
+import { IonHeader, IonContent, IonButton, IonFooter } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonContent, IonButton, IonFooter],
 })
+//aqui va toda la logica de la pagina
+//variables y metodos
 export class HomePage {
+  public numero: number = 0;
   constructor() {}
 }
